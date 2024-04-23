5b37c19fa54526b5cde854ea622147c78c4aa968
ALFRED <alfredjhonatan@gmail.com>
2024-04-23 11:21:05 +0700

    Initial Commit

---
+++ Color/ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp.swift
+//  Color
+//
+//  Created by Alfred Jhonatan on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
